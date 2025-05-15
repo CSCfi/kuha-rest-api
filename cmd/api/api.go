a663ac43e5fdba14d257602d23b2bf3dcd978269
Dean Ruina <dean.ruina@csc.fi>
2025-05-15 14:31:47 +0300

    Graceful Shutdown

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/DeRuina/KUHA-REST-API/docs" // This is required to generate swagger docs
@@ -166,6 +171,35 @@ func (app *api) run(mux http.Handler) error {
 		IdleTimeout:  time.Minute,
 	}
 
-	logger.Logger.Infow("server started", "addr", app.config.addr, "env", app.config.env)
-	return srv.ListenAndServe()
+	shutdown := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		logger.Logger.Infow("signal caught", "signal", s.String())
+
+		shutdown <- srv.Shutdown(ctx)
+	}()
+
+	logger.Logger.Infow("server has started", "addr", app.config.addr, "env", app.config.env)
+
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdown
+	if err != nil {
+		return err
+	}
+
+	logger.Logger.Infow("server has stopped", "addr", app.config.addr, "env", app.config.env)
+
+	return nil
 }
