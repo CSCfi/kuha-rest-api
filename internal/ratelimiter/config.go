5448dce0c63cbcf1b52159fd2a20c67ed2b8a2f4
Dean Ruina <dean.ruina@csc.fi>
2025-08-18 14:59:03 +0300

    fix: raise ratelimit for utv

--- internal/ratelimiter/config.go
+++ internal/ratelimiter/config.go
@@ -8,10 +8,10 @@ var RoleLimits = map[string]struct {
 }{
 	"admin":      {Limit: 1000, Window: time.Minute},
 	"fis":        {Limit: 100, Window: time.Minute},
-	"utv":        {Limit: 100, Window: time.Minute},
-	"kamk":       {Limit: 300, Window: time.Minute},
+	"utv":        {Limit: 500, Window: time.Minute},
+	"kamk":       {Limit: 500, Window: time.Minute},
 	"klab":       {Limit: 100, Window: time.Minute},
-	"tietoevry":  {Limit: 300, Window: time.Minute},
+	"tietoevry":  {Limit: 500, Window: time.Minute},
 	"coachtech":  {Limit: 100, Window: time.Minute},
 	"archinisis": {Limit: 100, Window: time.Minute},
 	"default":    {Limit: 100, Window: time.Minute},
