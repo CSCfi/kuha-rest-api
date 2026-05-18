be7eed2998da8ee1252d5a9f8021e7967936ad25
GitHub Action <action@github.com>
2026-05-18 11:49:56 +0000

    Update version to 1.3.5

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/DeRuina/KUHA-REST-API/internal/store/cache"
 )
 
-const version = "1.3.4"
+const version = "1.3.5"
 
 //	@title			KUHA REST API
 //	@description	API for integrating, analyzing, and visualizing sports and health data
